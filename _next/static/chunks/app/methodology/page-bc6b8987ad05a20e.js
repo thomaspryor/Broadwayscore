30a22570eb1ac738bd7de8d727fd076695cd1e38
thomaspryor <thomas.pryor@gmail.com>
2026-01-21 02:54:56 +0000

    Deploying to gh-pages from @ thomaspryor/Broadwayscore@31e126d896379af0d93a3800159a7959c2fabdba 🚀

---
+++ _next/static/chunks/app/methodology/page-bc6b8987ad05a20e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[949,14,864],{6772:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=6772)}),_N_E=n.O()}]);
